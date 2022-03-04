1f8ed543c7dd684afa6c3d56e9e76d2478d74c23
Shuvoghosh7 <shuvoghosh1610@gmail.com>
2022-03-04 12:29:25 +0600

    alert,conferm,promot add

---
+++ alert/alert.js
@@ -0,0 +1,12 @@
+
+const macomming = () =>{
+    alert('alert is going')
+}
+const isGoing = ()=>{
+   const response = confirm('lets got to watch movie')
+   if(response === true){
+       alert('give me money')
+   }else{
+       console.log('Dore giya mor')
+   }
+}
